864a32cf51084cf03f44e8bb55e415a4e8076f6e
christina.t <christina.t@omgroupinc.us>
2021-05-03 15:11:58 -0400

    Initial Commit

---
+++ src/com/test/mathFunctions.java
@@ -0,0 +1,41 @@
+package com.test;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class mathFunctions {
+	
+	@BeforeClass(alwaysRun = true)
+	private void login() {
+		System.out.println("Login to App");
+		
+	}
+	@BeforeMethod(alwaysRun = true)
+	public void enterURL() {
+		System.out.println("Google Title Test");
+	}
+	@Test
+	public void googelTitleTest() {
+		System.out.println("Google Title Test");
+	}
+	
+	@AfterMethod
+	public void logout() {
+		System.out.println("logout from app");
+	}
+	
+	@AfterTest
+	public void deleteAllCookies() {
+		System.out.println("delete All Cookies");
+	}
+	
+	@AfterClass
+	public void closeBrowser() {
+		System.out.println("Close Browser");
+	}
+
+}

93e00e203a6bd03c07b82736ea5ce0821d3c5870
christina.t <christina.t@omgroupinc.us>
2021-05-03 18:33:47 -0400

    Added a comment

--- src/com/test/mathFunctions.java
+++ src/com/test/mathFunctions.java
@@ -8,34 +8,37 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class mathFunctions {
-	
+
 	@BeforeClass(alwaysRun = true)
 	private void login() {
 		System.out.println("Login to App");
-		
+
 	}
+
 	@BeforeMethod(alwaysRun = true)
 	public void enterURL() {
 		System.out.println("Google Title Test");
 	}
+
 	@Test
 	public void googelTitleTest() {
 		System.out.println("Google Title Test");
 	}
-	
+
 	@AfterMethod
 	public void logout() {
 		System.out.println("logout from app");
 	}
-	
+
 	@AfterTest
 	public void deleteAllCookies() {
 		System.out.println("delete All Cookies");
 	}
-	
+
 	@AfterClass
 	public void closeBrowser() {
 		System.out.println("Close Browser");
 	}
+	/************ End ******************/
 
 }
